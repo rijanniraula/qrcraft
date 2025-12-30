8f77c1428036d65233420e3b18dc2c3c0072a269
rijanniraula <rijann38@gmail.com>
2025-12-30 19:37:08 +0545

    custom select component

---
+++ src/components/common/SelectCustom.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from "../ui/select";
+import { cn } from "@/lib/utils";
+
+const SelectCustom = ({
+  options,
+  className,
+  defaultValue,
+  onChange,
+}: {
+  options: any;
+  defaultValue?: string;
+  className?: string;
+  onChange?: (value: string) => void;
+}) => {
+  return (
+    <Select onValueChange={onChange} defaultValue={defaultValue}>
+      <SelectTrigger className={cn("w-full rounded-xl", className)}>
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent
+        className="text-sm bg-card rounded-xl overflow-hidden"
+        position="popper"
+      >
+        {options.map((option: any) => (
+          <SelectItem
+            key={option.value}
+            value={option.value}
+            className="text-sm font-medium rounded-xl"
+          >
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SelectCustom;
